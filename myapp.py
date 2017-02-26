2f450110821343f346fe0197283768d85478fdd1
entering <me@eduardooliveira.net>
2017-02-26 22:22:03 +0100

    Remove notes

--- myapp.py
+++ myapp.py
@@ -4,11 +4,6 @@ import cherrypy
 import home.apps
 import dashboard.apps
 
-class Notes(object):
-    @cherrypy.expose
-    def index(self):
-        return "Notes!"
-
 if __name__ == '__main__':
     cherrypy.config.update("server.conf")
     home.apps.Home()
