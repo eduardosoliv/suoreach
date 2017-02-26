7ba64e0ce69b2adf83a39ca63363ef3c791bcf8e
entering <me@eduardooliveira.net>
2017-02-26 16:41:55 +0100

    Added static configuration

--- myapp.py
+++ myapp.py
@@ -1,3 +1,4 @@
+import os
 import cherrypy
 
 class Root(object):
@@ -16,10 +17,31 @@ class Notes(object):
         return "Notes!"
 
 if __name__ == '__main__':
-   cherrypy.config.update("server.conf")
-   cherrypy.tree.mount(Root(), '/')
-   cherrypy.tree.mount(Dashboard(), '/dashboard')
-   cherrypy.tree.mount(Notes(), '/notes')
+    static_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static')
 
-   cherrypy.engine.start()
-   cherrypy.engine.block()
+    root_config = {
+        '/static/css': {
+            'tools.gzip.on': True,
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': os.path.join(static_dir, 'css'),
+            'tools.gzip.mime_types':['text/css']
+        },
+        '/static/js': {
+            'tools.gzip.on': True,
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': os.path.join(static_dir, 'js'),
+            'tools.gzip.mime_types':['application/javascript']
+        },
+        '/favicon.ico': {
+            'tools.staticfile.on': True,
+            'tools.staticfile.filename': os.path.join(static_dir, 'favicon.ico')
+        }
+    }
+
+    cherrypy.config.update("server.conf")
+    cherrypy.tree.mount(Root(), '/', root_config)
+    cherrypy.tree.mount(Dashboard(), '/dashboard')
+    cherrypy.tree.mount(Notes(), '/notes')
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
